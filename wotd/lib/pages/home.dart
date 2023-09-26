4cc77865b69d7da350767eacca7b8876e173fdc4
Lucas <48874219+LucasVlad@users.noreply.github.com>
2023-09-26 11:40:32 -0400

    Added Dummy API

---
+++ wotd/lib/pages/home.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:wotd/user_model.dart';
+import 'package:wotd/api_service.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late List<UserModel>? _userModel = [];
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  void _getData() async {
+    _userModel = (await ApiService().getUsers())!;
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Today'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(35, 35, 35, 1),
+      ),
+      body: _userModel == null || _userModel!.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _userModel!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(_userModel![index].id.toString()),
+                          Text(_userModel![index].username),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(_userModel![index].email),
+                          Text(_userModel![index].website),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+      backgroundColor: const Color.fromRGBO(35, 35, 35, 1),
+    );
+  }
+}
