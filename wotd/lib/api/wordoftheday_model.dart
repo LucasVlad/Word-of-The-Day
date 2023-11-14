2e07119b071718d737ac874db2cde9fa8add3f03
Lucas <48874219+LucasVlad@users.noreply.github.com>
2023-11-13 20:28:37 -0500

    Created Wordnik WordOfTheDay API Model

--- wotd/lib/api/wordoftheday_model.dart
+++ wotd/lib/api/wordoftheday_model.dart
@@ -4,4 +4,36 @@
 
 import 'dart:convert';
 
-class wordOfTheDay {}
+List<WordOfTheDay> wordOfTheDayFromJson(String str) => List<WordOfTheDay>.from(
+    json.decode(str).map((x) => WordOfTheDay.fromJson(x)));
+
+String wordOfTheDayToJson(List<WordOfTheDay> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class WordOfTheDay {
+  String word;
+  String partOfSpeech;
+  String definition;
+  String exampleSentence;
+
+  WordOfTheDay({
+    required this.word,
+    required this.partOfSpeech,
+    required this.definition,
+    required this.exampleSentence,
+  });
+
+  factory WordOfTheDay.fromJson(Map<String, dynamic> json) => WordOfTheDay(
+        word: json["word"],
+        partOfSpeech: json["definitions[0].partOfSpeech"],
+        definition: json["definitions[0].text"],
+        exampleSentence: json["examples[0].text"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "word": word,
+        "partOfSpeech": partOfSpeech,
+        "definition": definition,
+        "exampleSentence": exampleSentence,
+      };
+}
